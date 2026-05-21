866e6e4b2f46287b350027a94c5920d27923e63a
tglam03 <nguyenductunglam94@gmail.com>
2026-05-21 16:10:59 +0700

    New project assignment

---
+++ src/LecturerSystem/model/GiangVien.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LecturerSystem.model;
+
+/**
+ *
+ * @author Minh Thư
+ */
+public class GiangVien {
+    
+}
