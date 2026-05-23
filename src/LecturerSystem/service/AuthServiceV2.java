bf332963363f988c03580c7292f49025466379ff
NhuQuynh28 <lenhuquynh28012006@gmail.com>
2026-05-23 22:58:48 +0700

    Create new AuthService and LoginFrame

---
+++ src/LecturerSystem/service/AuthServiceV2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LecturerSystem.service;
+
+/**
+ *
+ * @author T14 GEN2
+ */
+public class AuthServiceV2 {
+    
+}
